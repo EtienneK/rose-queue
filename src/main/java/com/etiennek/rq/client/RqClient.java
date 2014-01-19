ba43a16a723cfe0bbfe44fe8b4f7aa35aec05d98
Etienne Koekemoer <e@ek.pe>
2014-01-19 16:48:34 +0200

    working on client

--- src/main/java/com/etiennek/rq/client/RqClient.java
+++ src/main/java/com/etiennek/rq/client/RqClient.java
@@ -1,18 +1,15 @@
 package com.etiennek.rq.client;
 
 import java.util.NoSuchElementException;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import javax.ws.rs.client.AsyncInvoker;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.InvocationCallback;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.MessageBodyReader;
 import javax.ws.rs.ext.MessageBodyWriter;
 
@@ -26,66 +23,128 @@ import rx.Subscription;
 
 import com.etiennek.rq.api.QueueService;
 import com.etiennek.rq.api.dtos.Message;
+import com.etiennek.rq.server.providers.ObjectMapperProvider;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import com.google.common.base.Optional;
-import com.google.common.base.Throwables;
 
-public class RqClient implements QueueService {
+public class RqClient implements QueueService, AutoCloseable {
 
   private Client client;
   private WebTarget baseWebTarget;
 
-  public RqClient(int port) {
-    client = ClientBuilder.newClient(new ClientConfig().connectorProvider(
-        new JettyConnectorProvider()).register(JacksonJaxbJsonProvider.class,
-        MessageBodyReader.class, MessageBodyWriter.class));
-
-    baseWebTarget = client.target("http://localhost:" + port);
+  public RqClient(String baseUri) {
+    client = ClientBuilder.newClient(
+        new ClientConfig().connectorProvider(new JettyConnectorProvider()).register(JacksonJaxbJsonProvider.class,
+            MessageBodyReader.class, MessageBodyWriter.class)).register(ObjectMapperProvider.class);
+    baseWebTarget = client.target(baseUri);
   }
 
   @Override
   public Observable<Optional<Message>> hold(String queueName, int secondsToHold) {
 
     final AsyncInvoker asyncInvoker = baseWebTarget.path("queues").path(queueName)
-        .queryParam("secondsToHold", secondsToHold).request(MediaType.APPLICATION_JSON_TYPE)
-        .async();
+        .queryParam("secondsToHold", secondsToHold).request(MediaType.APPLICATION_JSON_TYPE).async();
 
-    try {
-      asyncInvoker.get(new InvocationCallback<Message>() {
+    return Observable.create(new OnSubscribeFunc<Optional<Message>>() {
 
-        public void completed(Message message) {
-          System.out.println(message);
-        }
+      @Override
+      public Subscription onSubscribe(final Observer<? super Optional<Message>> t1) {
+        final Future<Optional<Message>> future = asyncInvoker.get(new InvocationCallback<Optional<Message>>() {
 
-        public void failed(Throwable throwable) {
-          throwable.printStackTrace();
-        }
-      });
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+          public void completed(Optional<Message> message) {
+            t1.onNext(message);
+            t1.onCompleted();
+          }
 
-    return null;
+          public void failed(Throwable throwable) {
+            t1.onError(throwable);
+          }
+
+        });
+
+        return new Subscription() {
+          @Override
+          public void unsubscribe() {
+            future.cancel(true);
+          }
+        };
+
+      }
+    });
   }
 
   @Override
-  public Observable<Void> add(String queueName, Message message) {
-    WebTarget webTarget = baseWebTarget.path("queues").path(queueName);
+  public Observable<Void> add(String queueName, final Message message) {
+    final AsyncInvoker asyncInvoker = baseWebTarget.path("queues").path(queueName)
+        .request(MediaType.APPLICATION_JSON_TYPE).async();
 
-    Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON_TYPE);
-    Response response = invocationBuilder.post(Entity.entity(message,
-        MediaType.APPLICATION_JSON_TYPE));
+    return Observable.create(new OnSubscribeFunc<Void>() {
+      @Override
+      public Subscription onSubscribe(final Observer<? super Void> t1) {
 
-    System.out.println(response.getStatus());
-    System.out.println(response.readEntity(Void.class));
+        final Future<Void> future = asyncInvoker.post(Entity.entity(message, MediaType.APPLICATION_JSON_TYPE),
+            new InvocationCallback<Void>() {
 
-    return null;
+              @Override
+              public void completed(Void response) {
+                t1.onNext(response);
+                t1.onCompleted();
+              }
+
+              @Override
+              public void failed(Throwable throwable) {
+                t1.onError(throwable);
+              }
+            });
+
+        return new Subscription() {
+          @Override
+          public void unsubscribe() {
+            future.cancel(true);
+          }
+        };
+
+      }
+    });
   }
 
   @Override
   public Observable<Void> deleteHeldMessage(String id) throws NoSuchElementException {
-    // TODO Auto-generated method stub
-    return null;
+    final AsyncInvoker asyncInvoker = baseWebTarget.path("queues").path("heldMessage").path(id)
+        .request(MediaType.APPLICATION_JSON_TYPE).async();
+
+    return Observable.create(new OnSubscribeFunc<Void>() {
+      @Override
+      public Subscription onSubscribe(final Observer<? super Void> t1) {
+
+        final Future<Void> future = asyncInvoker.delete(new InvocationCallback<Void>() {
+
+          @Override
+          public void completed(Void response) {
+            t1.onNext(response);
+            t1.onCompleted();
+          }
+
+          @Override
+          public void failed(Throwable throwable) {
+            t1.onError(throwable);
+          }
+        });
+
+        return new Subscription() {
+          @Override
+          public void unsubscribe() {
+            future.cancel(true);
+          }
+        };
+
+      }
+    });
+  }
+
+  @Override
+  public void close() {
+    client.close();
   }
 
 }
--- src/test/java/com/etiennek/rq/client/RqClientTest.java
+++ src/test/java/com/etiennek/rq/client/RqClientTest.java
@@ -1,23 +1,38 @@
 package com.etiennek.rq.client;
 
+import static com.jayway.awaitility.Awaitility.await;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import junit.framework.Assert;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import rx.util.functions.Action1;
+
 import com.etiennek.rq.api.dtos.Message;
 import com.etiennek.rq.server.RqApplication;
+import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
 
 public class RqClientTest {
 
-  private static final int port = 7357;
+  private static final int PORT = 7357;
+  private static final String BASE_URI = "http://localhost:" + PORT;
+
+  private static RqClient client;
 
   @BeforeClass
   public static void initStatic() throws Exception {
+    client = new RqClient(BASE_URI);
     new Thread() {
       public void run() {
         try {
-          RqApplication.start(port);
+          RqApplication.start(PORT);
         } catch (Exception e) {
           Throwables.propagate(e);
         }
@@ -29,13 +44,62 @@ public class RqClientTest {
   @AfterClass
   public static void destroyStatic() throws Exception {
     RqApplication.stop();
+    client.close();
   }
 
   @Test
-  public void test() {
-    RqClient rqClient = new RqClient(port);
-    rqClient.add("SomeQueue", new Message("Some string body"));
-    rqClient.hold("SomeQueue", 60);
-  }
+  public void test() throws InterruptedException {
+    final AtomicInteger count = new AtomicInteger();
+    final int numberOfRuns = 1;
 
+    final String queueName = "test.queue";
+    final String messageBody = "This is a test message you all :D";
+
+    final Action1<Optional<Message>> sequence4 = new Action1<Optional<Message>>() {
+      @Override
+      public void call(Optional<Message> t1) {
+        Assert.assertFalse(t1.isPresent());
+        count.incrementAndGet();
+      }
+    };
+
+    final Action1<Void> sequence3 = new Action1<Void>() {
+      @Override
+      public void call(Void t1) {
+        try {
+          Thread.sleep(3000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        client.hold(queueName, 60).subscribe(sequence4);
+      }
+    };
+
+    final Action1<Optional<Message>> sequence2 = new Action1<Optional<Message>>() {
+      @Override
+      public void call(Optional<Message> t1) {
+        Assert.assertEquals(messageBody, t1.get().getMessageBody());
+        client.deleteHeldMessage(t1.get().getId()).subscribe(sequence3);
+      }
+    };
+
+    final Action1<Void> sequence1 = new Action1<Void>() {
+      @Override
+      public void call(Void t1) {
+        client.hold(queueName, 2).subscribe(sequence2);
+      }
+    };
+
+    for (int i = 0; i < numberOfRuns; ++i) {
+      client.add(queueName, new Message(messageBody)).subscribe(sequence1);
+    }
+
+    await().atMost(4, SECONDS).until(new Callable<Boolean>() {
+      @Override
+      public Boolean call() throws Exception {
+        return count.get() >= numberOfRuns;
+      }
+    });
+
+  }
 }
